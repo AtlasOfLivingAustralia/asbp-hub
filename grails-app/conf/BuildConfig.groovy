58c856e7ece565721fcc966474eed28e2cb88561
Peter Ansell <p_ansell@yahoo.com>
2016-09-21 15:14:40 +1000

    Remove ala-auth

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -67,7 +67,6 @@ grails.project.dependency.resolution = {
         //runtime ":zipped-resources:1.0.1"
         //runtime ":cached-resources:1.1"
         //runtime ":yui-minify-resources:0.1.5"
-        compile ':ala-auth:1.3.4'        
         runtime ":biocache-hubs:0.75"
         runtime ":ala-web-theme:0.8.1"
     }
