6ab48d1d1fb2890bb55926a9dc46eb3e684026ef
Peter Ansell <p_ansell@yahoo.com>
2016-10-06 09:34:02 +1100

    Attempt to make the same changes here that worked for amrin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -67,7 +67,8 @@ grails.project.dependency.resolution = {
         //runtime ":zipped-resources:1.0.1"
         //runtime ":cached-resources:1.1"
         //runtime ":yui-minify-resources:0.1.5"
-        runtime ":biocache-hubs:0.75"
-        runtime ":ala-web-theme:0.8.1"
+        runtime ":biocache-hubs:1.2-SNAPSHOT"
+        runtime ":ala-bootstrap2:2.4.3-SNAPSHOT"
+        //runtime ":ala-web-theme:0.8.1"
     }
 }
