19c10c20278e92ab52f3beb2df5a02a88dfb19c6
Patricia Koh <patricia.koh@csiro.au>
2018-05-17 11:41:28 +1000

    Layout fixes

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -5,13 +5,9 @@
 // You're free to add application-wide JavaScript to this file, but it's generally better
 // to create separate JavaScript files as needed.
 //= require jquery-1.11.1.js
-//= require biocache-hubs.js
 //= require hubCore.js
 //= require biocache-hubs.js
-//require_jquery.autocomplete.js
 //= require bootstrap.min.js
-//
-
 
 if (typeof jQuery !== 'undefined') {
     (function($) {
