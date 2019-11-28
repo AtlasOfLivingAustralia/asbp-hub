4143e9f05ec0adbfc684c16c491f4f80caa3d74c
Nick dos Remedios <nick.dosremedios@csiro.au>
2019-11-28 15:31:28 +1100

    Fixes for downloads and autocomplete

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -5,6 +5,7 @@
 // You're free to add application-wide JavaScript to this file, but it's generally better
 // to create separate JavaScript files as needed.
 //= require jquery-1.11.1.js
+//= require autocomplete/autocomplete-1.12.1.js
 //= require hubCore.js
 //= require biocache-hubs.js
 //= require bootstrap.min.js
