3faef5a3697821b7e9b26166e1660010cc513bdb
Jack Brinkman <jack.brinkman@csiro.au>
2022-05-13 15:28:40 +1000

    #7 Moved navbar into own template file, removed testing js file

---
+++ grails-app/assets/javascripts/seedpartnership/_all.js
@@ -0,0 +1,14 @@
+
+//= require jquery.min.js
+//= require jquery-migrate.min.js
+//= require jquery.smartmenus.min.js
+//= require webpack-pro.runtime.min.js
+//= require webpack.runtime.min.js
+//= require frontend-modules.min.js
+//= require hooks.min.js
+//= require i18n.min.js
+//= require elementor-pro-config.js
+//= require frontend.pro.min.js
+//= require elementor-config.js
+//= require frontend.min.js
+//= require preloaded-elements-handlers.min.js
